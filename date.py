17cbb8eebf38eb7be93c8ccb92ff084733fdfc86
Maciej Kozłowski <maciejmaciejkoz@gmail.com>
2021-12-05 14:03:20 +0100

    Add comparison method in Date class

--- date.py
+++ date.py
@@ -6,13 +6,31 @@ class Date:
         self._month = month
         self._year = year
     
-    def __gt__(self, other):
+    def __ge__(self, other) -> bool:
         if self._year == other._year:
-            
+            if self._month == other._month:
+                if self._day == other._day:
+                    if self._hour == other._hour:
+                        if self._minutes >= other._minutes:
+                            return True
+                        else:
+                            return False
+                    elif self._hour > other._hour:
+                        return True
+                    else:
+                        return False
+                elif self._day > other._day:
+                    return True
+                else:
+                    return False
+            elif self._month > other._month:
+                return True
+            else:
+                return False
         elif self._year > other._year:
             return True
         else:
             return False
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self._hour}:{self._minutes} {self._day}.{self._month}.{self._year}"
--- person.py
+++ person.py
@@ -1,5 +1,5 @@
 class Person:
-    def __init__(self, name: str, surname: str, contact_list = None) -> None:
+    def __init__(self, name: str, surname: str, contact_list: list = None) -> None:
         self._name = name
         self._surname = surname
         self._contact_list = contact_list
