ce8b238b0c30b0aa972c6672bead454311a072d5
Maciej Kozłowski <maciejmaciejkoz@gmail.com>
2021-12-19 21:26:22 +0100

    Add documentation

--- disease.py
+++ disease.py
@@ -5,7 +5,18 @@ class InvalidInfectiousPeriodError(Exception):
         super().__init__(f"{period} is not a valid time.") 
 
 class Disease:
+    '''
+    Class Disease. Conatins attributes:
+    :param name: Disease's name
+    :type name: str
+
+    :param infectious_period: Disease's infectious period
+    :type infectious_period: timedelta
+    '''
     def __init__(self, name: str, infectious_period: int) -> None:
+        '''
+        Initalizes disease object
+        '''
         self._name = str(name)
         try:
             self._infectious_period = timedelta(minutes=infectious_period)
@@ -13,7 +24,13 @@ class Disease:
             raise InvalidInfectiousPeriodError(infectious_period)
 
     def get_disease_name(self) -> str:
+        '''
+        Returns disease's name
+        '''
         return self._name
 
     def get_infectious_period(self) -> str:
+        '''
+        Returns disease's infectious period
+        '''
         return self._infectious_period
--- person.py
+++ person.py
@@ -17,12 +17,26 @@ class InvalidDiseaseError(Exception):
 
 
 class Person:
+    '''
+    Class Person. Conatins attributes:
+    :param name: Person's name
+    :type name: str
+
+    :param surname: Person's surname
+    :type surname: str
+
+    :param meeting_list: Person's meeting list
+    :type meeting_list: list
+    '''
     def __init__(self, name: str, surname: str) -> None:
         self._name = str(name)
         self._surname = str(surname)
         self._meeting_list = []
 
     def add_meeting(self, person, date: str, duration: int) -> None:
+        '''
+        Adds new meeting to person's meeting list.
+        '''
         if not isinstance(person, Person):
             raise InvalidPersonError(person)
 
@@ -32,6 +46,7 @@ class Person:
         except ValueError:
             raise InvalidMeetingDateError(date, duration)
 
+        # Creating an universally unique identifier for a meeting
         meeting_uuid = uuid.uuid4()
 
         self._meeting_list.append(
@@ -53,9 +68,15 @@ class Person:
         )
 
     def meetings(self) -> list:
+        '''
+        Returns person's meeting list
+        '''
         return self._meeting_list
 
     def who_is_infected(self, disease: Disease, last_meeting_date: datetime = None, last_meeting_duration: timedelta = None, checked_meetings: list = None, infected_people: list = None) -> set:
+        '''
+        Return a list of possibly infected people.
+        '''
         if not isinstance(disease, Disease):
             raise InvalidDiseaseError(disease)
         
@@ -77,4 +98,7 @@ class Person:
         return infected_people
 
     def __str__(self) -> str:
+        '''
+        Returns a string "name surname"
+        '''
         return f"{self._name} {self._surname}"
