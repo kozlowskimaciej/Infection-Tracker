740ba8995fc0f9d68e03817e9195101512adeb66
Maciej Kozłowski <maciejmaciejkoz@gmail.com>
2021-12-21 15:35:57 +0100

    Add reading from csv files

---
+++ files.py
@@ -0,0 +1,19 @@
+import csv
+from person import Person
+
+def read_meetings_csv(file):
+    with open(file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        people = dict()
+        for row in reader:
+            person1 = str(row['Name_1'] + ' ' + row['Surname_1'])
+            if person1 not in people:
+                people[person1] = Person(row['Name_1'], row['Surname_1'])
+
+            person2 = str(row['Name_2'] + ' ' + row['Surname_2'])
+            if person2 not in people:
+                people[person2] = Person(row['Name_2'], row['Surname_2'])
+            
+            people[person1].add_meeting(people[person2], row['Date'], int(row['Duration']))
+
+        return people
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from person import Person
+from disease import Disease
+from files import read_meetings_csv
 
 def main():
-    pass
+    people = read_meetings_csv('example_data/meetings.csv')
+    covid = Disease("covid", 100)
+    print(people['Dexter Thomas'].who_is_infected(covid))
 
 if __name__ == "__main__":
     main()
